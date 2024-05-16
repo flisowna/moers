65dda4eec7ee5bfadb1dc14364b28f3cc82107b6
flisowna <ewa.agnieszka.flis@gmail.com>
2024-05-16 14:24:45 +0200

    navbar and format

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -10,20 +10,24 @@ export function Navbar() {
       <nav className="p-4 text-slate-800 bg-[#F8B200] md:px-16 px-4 md:py-8 border-b-2 border-white uppercase flex items-center justify-between">
         <div>
           <a className={`mr-4 ${pathname === "/" ? "text-white border-b" : ""}`} href="/">
-            Chat
+            Chatbot
           </a>
-          <a className={`mr-4 ${pathname === "/structured_output" ? "text-white border-b" : ""}`} href="/structured_output">
+          {/* <a className={`mr-4 ${pathname === "/structured_output" ? "text-white border-b" : ""}`} href="/structured_output">
             Structured Output
-          </a>
-          <a className={`mr-4 ${pathname === "/agents" ? "text-white border-b" : ""}`} href="/agents">
+          </a> */}
+          {/* <a className={`mr-4 ${pathname === "/agents" ? "text-white border-b" : ""}`} href="/agents">
             Agents
+          </a> */}
+          
+          <a className={`mr-4 ${pathname === "/dokumentensuche" ? "text-white border-b" : ""}`} href="/retrieval">
+            Dokumentensuche
           </a>
           <a className={`mr-4 ${pathname === "/retrieval" ? "text-white border-b" : ""}`} href="/retrieval">
-            Retrieval
+            Upload
           </a>
-          <a className={`mr-4 ${pathname === "/retrieval_agents" ? "text-white border-b" : ""}`} href="/retrieval_agents">
+          {/* <a className={`mr-4 ${pathname === "/retrieval_agents" ? "text-white border-b" : ""}`} href="/retrieval_agents">
             Retrieval Agents
-          </a>
+          </a> */}
         </div>
         <div className="absolute right-4 md:right-16 top-9">
           <Image src="/images/logo_moers.svg" alt="Logo" width={246} height={84} />
